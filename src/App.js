9089e7f2b4101d8b8db700b8d65b132d26d4a707
Your Name <thales_so@hotmail.com>
2021-07-06 15:56:12 -0300

    finished

--- src/App.js
+++ src/App.js
@@ -61,6 +61,12 @@ export default () => {
         Direitos de imagem para <strong>Netflix</strong><br/>
         Dados pegos do site <strong>Themoviedb.org</strong>
       </footer>
+
+      {movieList.length <= 0 &&
+      <div className="loading">
+        <img src="https://media.wired.com/photos/592744d3f3e2356fd800bf00/master/w_2560%2Cc_limit/Netflix_LoadTime.gif" alt="Carregando" />
+      </div>
+      }
     </div>
   );
 }
--- src/components/MovieRow/index.js
+++ src/components/MovieRow/index.js
@@ -1,16 +1,27 @@
-import React from 'react';
+import React, { useState }from 'react';
 import './styles.css';
 import NavigateBeforeIcon from '@material-ui/icons/NavigateBefore';
 import NavigateNextIcon from '@material-ui/icons/NavigateNext';
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default ({title, items}) => {
+  const [scrollX, setScrollX] = useState(0);
+
   const handleLeftArrow = () => {
-    
+    let x = scrollX + Math.round(window.innerWidth / 2);
+    if(x > 0) {
+      x = 0;
+    }
+    setScrollX(x);
   }
 
   const handleRightArrow = () => {
-
+    let x = scrollX - Math.round(window.innerWidth / 2);
+    let listW = items.results.length * 150;
+    if((window.innerWidth - listW) > x) {
+      x = (window.innerWidth - listW) - 60;
+    }
+    setScrollX(x);
   }
   
   return (
@@ -23,7 +34,10 @@ export default ({title, items}) => {
         <NavigateNextIcon style={{fontSize: 50}} />
       </div>
       <div className="movieRow-list-area">
-        <div className="movieRow-list">
+        <div className="movieRow-list" style={{
+          marginLeft: scrollX,
+          width: items.results.length * 150
+        }}>
           {items.results.length > 0 && items.results.map((item, key)=>(
             <div key={key} className="movieRow-item">
               <img src={`https://image.tmdb.org/t/p/w300${item.poster_path}`} alt={item.original_tittle}/>
